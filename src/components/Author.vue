adeb2811a1b22a178ac4069a472eaac0ea825da6
rdang <41537401+rdangdev@users.noreply.github.com>
2022-09-11 13:33:46 +1000

    customised template
    initial description landing page

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -6,12 +6,11 @@
 		</h1>
 
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.
+			Hello.
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/suits_at">Follow on Twitter</a>
-			<a href="https://github.com/suits-at/netlifycms-gridsome">GitHub</a>
+			<a href="https://github.com/rdangdev">GitHub</a>
 		</p>
 
 	</div>
